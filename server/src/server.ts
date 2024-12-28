155f8480f068c1bfdafa6fdbed29c35c1263635f
baobao0303 <baobao22.work@gmail.com>
2024-12-28 13:52:54 +0700

    chore: Set up socket.io with Express

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,22 @@
-console.log("hello");
+const { createServer } = require("http");
+const express = require("express");
+const { Server } = require("socket.io");
+const mongoose = require("mongoose");
+
+const PORT = 4001;
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", () => {
+  console.log("User connected");
+});
+
+mongoose.connect("mongodb://localhost:27018/eltrello").then(() => {
+  console.log("Connected to MongoDB");
+
+  httpServer.listen(PORT, () => {
+    console.log(`API is listening on port ${PORT}`);
+  });
+});
